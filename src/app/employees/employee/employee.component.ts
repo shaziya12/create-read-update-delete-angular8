2a70711b2426d1d5826dfa678026829013dc66db
shaziya12 <ummimariya21@gmail.com>
2020-07-08 20:37:34 +0530

    2 files modified

--- src/app/employees/employee/employee.component.ts
+++ src/app/employees/employee/employee.component.ts
@@ -1,45 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
-import { EmployeeService } from '../../shared/employee.service';
+import { EmployeeService } from "../../shared/employee.service";
 
-import{ MatDialogRef } from '@angular/material/dialog';
-import { NotificationService } from '../../shared/notification.service';
+import { MatDialogRef } from "@angular/material/dialog";
+import { NotificationService } from "../../shared/notification.service";
 @Component({
-  selector: 'app-employee',
-  templateUrl: './employee.component.html',
-  
+  selector: "app-employee",
+  templateUrl: "./employee.component.html"
 })
 export class EmployeeComponent implements OnInit {
-
-  constructor(public service: EmployeeService,
- private notificationService: NotificationService,
-    public dialogref:MatDialogRef<EmployeeComponent>
-    ) { }
-
-
+  constructor(
+    public service: EmployeeService,
+    private notificationService: NotificationService,
+    public dialogref: MatDialogRef<EmployeeComponent>
+  ) {}
 
   ngOnInit() {
     this.service.getEmployees();
   }
 
-
   onClear() {
     this.service.form.reset();
     this.service.initializeFormGroup();
-    this.notificationService.success('Submitted successfully!');
+    this.notificationService.success("Submitted successfully!");
   }
 
   onSubmit() {
     if (this.service.form.valid) {
-      if (!this.service.form.get('$key').value)
+      if (!this.service.form.get("$key").value)
         this.service.insertEmployee(this.service.form.value);
-      else
-      this.service.updateEmployee(this.service.form.value);
+      else this.service.updateEmployee(this.service.form.value);
       this.service.form.reset();
       this.service.initializeFormGroup();
-     this.notificationService.success('Submitted successfully');
+      this.notificationService.success("Submitted successfully");
       this.onClose();
-     
     }
   }
 
@@ -48,5 +42,4 @@ export class EmployeeComponent implements OnInit {
     this.service.initializeFormGroup();
     this.dialogref.close();
   }
-
 }
